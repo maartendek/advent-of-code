41306c732483361daa62526b3ab7b5eb680210df
Maarten Dekker <maarten.dekker@capgemini.com>
2024-12-05 16:22:50 +0100

    use function to read file

--- day1/index.js
+++ day1/index.js
@@ -1,41 +1,23 @@
-import fs from "fs";
+import { adventFile } from "../lib/advent-file.js";
 
-export class AdventFile {
-  constructor(file) {
-    this.file = file;
-    this.row1 = [];
-    this.row2 = [];
-    this.result = 0;
-  }
+const row1 = [];
+const row2 = [];
+let result = 0;
 
-  read = () => {
-    return fs.readFile(this.file, "utf8", (err, data) => {
-      if (err) console.error(err);
+const file = await adventFile(1);
 
-      const lines = data.split(/\r\n|\n/);
-      lines.map((row) => {
-        const rowArr = row.split("   ");
-        this.row1.push(rowArr[0]);
-        this.row2.push(rowArr[1]);
-      });
+file.map((row) => {
+  const rowArr = row.split("   ");
+  row1.push(rowArr[0]);
+  row2.push(rowArr[1]);
+});
 
-      console.log("CALC");
+row1.sort();
+row2.sort();
 
-      this.row1.sort();
-      this.row2.sort();
-      console.log(this.row1);
+row1.map((item, index) => {
+  console.log(row1[index], row2[index], row1[index] - row2[index]);
+  result += Math.abs(row1[index] - row2[index]);
+});
 
-      this.row1.map((item, index) => {
-        console.log(
-          this.row1[index],
-          this.row2[index],
-          this.row1[index] - this.row2[index]
-        );
-        this.result += Math.abs(this.row1[index] - this.row2[index]);
-      });
-
-      console.log("result", this.result);
-      return this.result;
-    });
-  };
-}
+console.log("result", result);
