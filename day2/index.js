f3b6e7fbaae942bddee9148d582d73160854fcb8
Maarten Dekker <maarten.dekker@capgemini.com>
2024-12-06 15:19:42 +0100

    add day 2 - 2

--- day2/index.js
+++ day2/index.js
@@ -4,6 +4,14 @@ const reports = await adventFile(2);
 let safeReports = 0;
 
 reports.map((row) => {
+  if (rowIsSafe(row) || rowCanBeSafe(row)) {
+    safeReports++;
+  }
+});
+
+console.log("safeReports", safeReports);
+
+function rowIsSafe(row) {
   const arLevelDiffs = [];
   const levels = row.split(" ");
 
@@ -13,11 +21,21 @@ reports.map((row) => {
     }
   });
 
-  if (
+  return (
     arLevelDiffs.every((item) => item > 0 && item < 4) ||
     arLevelDiffs.every((item) => item < 0 && item > -4)
-  ) {
-    safeReports++;
+  );
+}
+
+function rowCanBeSafe(row) {
+  const levels = row.split(" ");
+
+  for (let index = 0; index < levels.length; index++) {
+    let possibleRow = levels.slice();
+    possibleRow.splice(index, 1);
+    if (rowIsSafe(possibleRow.join(" "))) {
+      return true;
+    }
   }
-});
-console.log("safeReports", safeReports);
+  return false;
+}
